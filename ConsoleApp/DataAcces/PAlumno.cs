3bed8d1012cf42c88ac1166ccf60ff3fb6a0e979
nicolaslopez1000 <40343837+nicolaslopez1000@users.noreply.github.com>
2019-03-19 20:11:40 -0300

    Se carga programa a GIT

---
+++ ConsoleApp/DataAcces/PAlumno.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class PAlumno
+    {
+        public bool insertAlumno(string connString)
+        {
+
+            try { 
+            SqlConnection context = new SqlConnection(connString);
+
+            using (context)
+            {
+                context.Open();
+                string nombre = "Pepe";
+                string apellido = "Loco";
+                short edad = 1;
+                string documento = "5.150.533-0";
+                SqlCommand cm = new SqlCommand();
+                    cm.Connection = context;
+
+                    cm.CommandText = "INSERT INTO Alumno (nombre, apellido , edad , documento) Values (@nom,@ap,@edad,@doc)";
+                    cm.Parameters.Add("@nom", SqlDbType.VarChar);
+                    cm.Parameters.Add("@ap", SqlDbType.VarChar);
+                    cm.Parameters.Add("@edad", SqlDbType.SmallInt);
+                    cm.Parameters.Add("@doc", SqlDbType.VarChar);
+
+                    cm.Parameters["@nom"].Value = nombre;
+                    cm.Parameters["@ap"].Value = apellido;
+                    cm.Parameters["@edad"].Value = edad;
+                    cm.Parameters["@doc"].Value = documento;
+
+                    cm.ExecuteNonQuery();
+
+                }
+            }
+            catch(Exception ex)
+            {
+                return false;
+
+            }
+            
+            
+                return true;
+        }
+
+
+    }
+}
---
+++ ConsoleApp/Liceo/Program.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liceo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            PAlumno pa = new PAlumno();
+
+            pa.insertAlumno(connection);
+
+        }
+    }
+}
